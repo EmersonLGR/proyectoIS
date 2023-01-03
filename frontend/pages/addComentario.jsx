76ed6c8b2c2d74ccc51c18d0caa8dedd96446686
EmersonLGR <114963787+EmersonLGR@users.noreply.github.com>
2023-01-03 07:40:45 -0300

    mejora visual

--- frontend/pages/addComentario.jsx
+++ frontend/pages/addComentario.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FormControl, FormLabel, FormHelperText, Input, Container, Heading, RadioGroup, HStack, Radio, Stack, Button, Textarea } from '@chakra-ui/react'
+import { FormControl, FormLabel, FormHelperText, Input, Container, Heading, RadioGroup, HStack, Radio, Stack, Button, Textarea, Box, Flex } from '@chakra-ui/react'
 import { createComentario } from '../data/comentarios'
 import TextareaInput from "../components/TextareaInput"
 import {useRouter} from 'next/router'
@@ -25,16 +25,39 @@ const addComentario = () => {
     }
 
     return(
-        <Container maxW="container.xl" mt={10}>
-            <Stack spacing={4} mt={10}>
-                <Heading as={"h1"} size={"2xl"} textAlign={"center"}>Crear comentario</Heading>
+        <Box
+        h='100vh'
+        bg='gray'
+        bgSize='cover'
+        > 
+        <Flex
+        direction='column'
+        alignItems='center'
+        justify='center'
+        h='100%'
+        bg='rgb(0 0 0 / 50%)'
+        p={['0 10%', null, '0 20%']}
+        > 
+           
+            <Heading 
+            color='white'
+            textTransform='uppercase'
+            textAlign="center"
+            fontWeight='light'
+            letterSpacing='5px'
+            >Crear comentario</Heading>
+             <Stack direction={{base: 'column', md: 'row'}}
+      spacing='40px'
+      mt='30px'
+      w={['100%', null, 'auto']}>
             <TextareaInput label="Descripción" handleChange={handleChange} name='description'/>
             </Stack>
             <HStack>
             <Button colorScheme={"green"} onClick={submitComentario}>Aceptar</Button>
             <Button colorScheme={"red"} onClick={()=>router.push('./comentarios')}>Cancelar</Button>
             </HStack>
-        </Container>
+            </Flex>
+        </Box>
     )
 }
 
